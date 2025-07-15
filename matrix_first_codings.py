5821647ab28a0a5ec4a0a7a2c850bd3779813c72
Raul Perez <raulpzs@outlook.com>
2025-07-15 15:02:05 -0600

    missing name

--- matrix_first_codings.py
+++ matrix_first_codings.py
@@ -119,6 +119,7 @@ def code_law(law_text):
         text={
                 "format": {
                     "type": "json_schema",
+                    "name": "CSO Law Coding",
                     "strict": True,
                     "schema": schema
                 }
